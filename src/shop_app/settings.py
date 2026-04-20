60391e06342ee8d5269bc2b8138c715520b3b315
Wiltort <wiltort21@gmail.com>
2026-04-20 14:07:07 +0500

    feat: add currencies

--- src/shop_app/settings.py
+++ src/shop_app/settings.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Доступные виды валют
+CURRENCIES = (
+    ('RUB', 'RUB'),
+    ('USD', 'USD'),
+)
