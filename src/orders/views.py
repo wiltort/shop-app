c13449f32772c01572899707e51a0a5163ec7b4f
Wiltort <wiltort21@gmail.com>
2026-04-21 13:16:06 +0500

    feat: add view for item

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+# from rest_framework.viewsets import ModelViewSet, APIViewSet
+from django.views.generic import DetailView
+
+from orders.models import Item
+
+
+class ItemView(DetailView):
+    model = Item
+    template_name = 'item.html'
