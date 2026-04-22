a7e5499a39faaf716be2221f9ffc3d3faf6bd592
Wiltort <wiltort21@gmail.com>
2026-04-22 12:04:34 +0500

    build: add static_url

--- src/shop_app/settings.py
+++ src/shop_app/settings.py
@@ -142,3 +142,4 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
